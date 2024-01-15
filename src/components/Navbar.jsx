fd06a2bf0ad59a34066add4c89f35b526521d385
Abdulmateen <alabiabdulmateen94@gmail.com>
2024-01-15 03:39:43 +0100

    started building the first part

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <h1 className=''>NETFLIX</h1>
+        <div>
+            <button>Sign In</button>
+            <button>Sign Up</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
