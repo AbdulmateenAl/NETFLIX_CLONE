e0ca82fb0ac681806f04c7fb00e492167c2dd818
Abdulhameed Bolaji Mustapha <abdulhameedmoonstapha735@gmail.com>
2024-03-22 12:09:08 +0100

    started displaying image on the home page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,14 +5,20 @@ import axios from 'axios';
 export const Main = () => {
     const [movies,setmovies] = useState([]);
 
+    const movie = movies[Math.floor(Math.random() * movies.length)];
+
     useEffect(() => {
         axios.get(requests.requestPopular).then((response) => {
-            setmovies(response.data)
+            setmovies(response.data.results)
         })
     }, [])
-    console.log(movies);
+    console.log(movie);
     
   return (
-    <div>main</div>
+    <div className='w-full h-[550px] text-white'>
+      <div className='w-full h-full'>
+        <img className='w-full h-full object-fit' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
+      </div>
+    </div>
   )
 }
